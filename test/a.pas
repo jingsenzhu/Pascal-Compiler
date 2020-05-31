ff0eb93ff3d1294d7703f7e813c17aebe6d95f77
txy791570042 <45891887+txy791570042@users.noreply.github.com>
2020-05-31 20:55:37 +0800

    Add files via upload

--- test/a.pas
+++ test/a.pas
@@ -1,31 +1,38 @@
-program a;
+program main;
 CONST
-    b = 1;
-    c = 2.2;
+    ONE = 1;
+    ONEPONE = 1.5;
+type
+    int = integer;
+    arr = array [0..3] of int;
+    rec = record a: integer; c: string; e: real; end;
 VAR
-    f : longint;
-    g : real;
-    h : boolean;
-    j : longint;
-    s : string;
-    x : array [0..3] of integer;
+    a : integer;
+    b : real;
+    c : boolean;
+    d : longint;
+    e : string;
+    f : char;
 
+procedure f1(k: int);
+CONST
+    t = 't';
+    tr = TRUE;
+type
+    arr2 = array[-2..2] of string;
+var
+    a1: arr;
+    a2: arr2;
 begin
-  x[2] := 2;
-  f := x[1];
-  if b = 1 then
-  begin
-      f := 6;
-  end
-  else begin
-      f := 9;
-  end;
-  readln(s);
-  writeln(x[2]);
-  writeln(x[1]);
-  writeln(s);
-  s := 'Hello world';
-  writeln(s);
-  s[0] := 'h';
-  writeln(s);
+end;
+
+procedure f2(k: boolean);
+type
+    rec2 = record a2: integer; c2: string; e2: real; end;
+var
+    r1: rec;
+    r2: rec2;
+begin
+end;
+begin
 end.
